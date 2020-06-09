7a87e1a545c19285a11a4b3c3a4856a51f2bb78a
CEduardoSQUTEC <eduardo.castro@utec.edu.pe>
2020-06-09 18:31:21 -0500

    ALU modified

--- L5_files_OPMUX_update/ALU.v
+++ L5_files_OPMUX_update/ALU.v
@@ -1,11 +1,13 @@
-module ALU(a,b,opcode,result);
+module alu(a,b,aluop,result);
     input [31:0] a,b;
-    input [3:0] opcode;
+    input [3:0] aluop;
     output [31:0] result;
     output flag;
 
-    //Idk como llamar a los modulos, porque necesitamos estructuras de control
-    arithmethic_op Arithmetic_part(a,b,opcode,result,flag);
-    logic_op Logic_part(a,b,opcode);
+    always @(aluop) begin
+        casex(aluop)
+            
+        endcase
+    end
 
 endmodule
