24d3f73f1cb06822fa7fb27b01155adcb281d0a6
Efraín Córdova <52300931+ecordova0975@users.noreply.github.com>
2020-07-04 21:18:54 -0500

    Some MIPS files added

--- scp/mips.v
+++ scp/mips.v
@@ -5,9 +5,9 @@ module mips(input           clk, reset,
             output  [31:0]  aluout, writedata,
             input   [31:0]  readdata);
 
-wire    memtoreg, branch, alusrc, alusrc, regdst, regwrite, jump;
-wire [2:0] alucontrol;
+  wire    memtoreg, branch, alusrc, alusrc, regdst, regwrite, jump;
+  wire [2:0] alucontrol;
 
-controller c(instr[31:26], instr[5:0], zero, memtoreg, memwrite, pcsrc, alusrc, regdst, regwrite, jump, alucontrol);
-datapath dp(clk, reset, memtoreg, pcsrc, alusrc, regdst, regwrite, jump, alucontrol, zero, pc, instr, aluout, writedata, readdata);
+  controller c(instr[31:26], instr[5:0], zero, memtoreg, memwrite, pcsrc, alusrc, regdst, regwrite, jump, alucontrol);
+  datapath dp(clk, reset, memtoreg, pcsrc, alusrc, regdst, regwrite, jump, alucontrol, zero, pc, instr, aluout, writedata, readdata);
 endmodule
