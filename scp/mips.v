a9a3d553a26b945fc3ce8e9d173a200142dc3ffd
CEduardoSQUTEC <eduardo.castro@utec.edu.pe>
2020-07-04 20:42:19 -0500

    aludec.v & mips.v

--- scp/mips.v
+++ scp/mips.v
@@ -8,13 +8,6 @@ module mips(input           clk, reset,
 wire    memtoreg, branch, alusrc, alusrc, regdst, regwrite, jump;
 wire [2:0] alucontrol;
 
-controller c(instr[31:26], instr[5:0], zero,
-memtoreg, memwrite, pcsrc,
-alusrc, regdst, regwrite, jump,
-alucontrol);
-datapath dp(clk, reset, memtoreg, pcsrc,
-alusrc, regdst, regwrite, jump,
-alucontrol,
-zero, pc, instr,
-aluout, writedata, readdata);
+controller c(instr[31:26], instr[5:0], zero, memtoreg, memwrite, pcsrc, alusrc, regdst, regwrite, jump, alucontrol);
+datapath dp(clk, reset, memtoreg, pcsrc, alusrc, regdst, regwrite, jump, alucontrol, zero, pc, instr, aluout, writedata, readdata);
 endmodule
