d80f1333e67cff32c100a49089ce580eb7f320a5
CEduardoSQUTEC <eduardo.castro@utec.edu.pe>
2020-07-07 19:40:22 -0500

    controller_tb in progress

--- mcp/controller_tb.v
+++ mcp/controller_tb.v
@@ -7,6 +7,9 @@ module controller_tb;
     wire [1:0] alusrcb, pcsrc;
     wire [2:0] alucontrol;
 
+    reg [14:0] vectornum, errors;
+    reg [30:0] testvector[11:0];
+
     controller dut(clk, reset,
                     op, funct,
                     zero, pcen,
@@ -15,6 +18,21 @@ module controller_tb;
                     alusrcb, pcsrc,
                     alucontrol);
 
+    always begin
+      clk = 1; #10; clk = 0; #10;
+    end
     
+    initial begin
+      $readmemh("controller.tv", testvector);
+      vectornum = 0; errors = 0;
+    end
+
+    always @(posedge clk) begin
+            op = testvector[vectornum][30:25];
+            funct = testvector[vectornum][23:18];
+            zero = testvector[vectornum][16];
+            result_expected = testvector[vectornum][14:0];
+    end
+
 
 endmodule
